d4de9a78ebe277f02c908835a9f36647a746faed
Anubhav Bhatt <anubhavbhatt@MacBookAir.lan>
2024-11-10 17:37:24 +0530

    Completed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import { PROJECTS } from "../constants";
+
 
 const Projects = () => {
   return (
@@ -7,7 +9,12 @@ const Projects = () => {
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+             whileInView={{opacity:1, x:0}}
+             initial={{opacity:0, x:-100}}
+             transition={{duration:1}}
+
+             className="w-full lg:w-1/4">
               <img
                 src={project.image}
                 alt={project.title}
@@ -15,8 +22,12 @@ const Projects = () => {
                 height={150}
                 className="mb-6 rounded "
               />
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+             whileInView={{opacity:1, x:0}}
+             initial={{opacity:0, x:100}}
+             transition={{duration:1}}
+             className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
               <div className="flex flex-wrap">
@@ -26,7 +37,7 @@ const Projects = () => {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,39 +2,49 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 
-
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. Through hands-on project experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full-stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
-export const EXPERIENCES = [
+export const CERTIFICATIONS = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2020 - 2021",
+    role: "Cybersecurity Roles, Processes & Operating System Security",
+    company: "Coursera",
+    description: `This certification provides an in-depth understanding of the various roles within cybersecurity, from security analyst to 
+    systems administrator. It covers foundational processes essential for managing security within organizations and explores best practices
+     for securing operating systems against threats, vulnerabilities, and attacks. .`,
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    role: "Oracle Database Foundations",
+    company: "Oracle",
+    description: `This course delves into the essential concepts and principles of database management with a focus on Oracle Database technology.`,
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: "2022 - 2023",
+    role: "Red Hat System Administration II (RH134)",
+    company: "Red hat",
+    description: `Building on RH124, this certification focuses on advanced Linux administration tasks.`,
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2022 - 2023",
+    role: "Red Hat System Administration I (RH124)",
+    company: "Red Hat",
+    description: `Focused on essential Linux skills, this course introduces the core concepts of Red Hat Enterprise Linux system administration.`,
+  },
+  {
+    year: "2022 - 2023",
+    role: "PCAP: Programming Essentials in Python: Cisco Networking Academy",
+    company: "Cisco Networking Academy",
+    description: `This certification covers programming fundamentals in Python, designed to build proficiency in one of the most popular and versatile programming languages.`,
+  },
+  {
+    year: "2022 - 2023",
+    role: "Web development Full stack ",
+    company: "Udemy",
+    description: `This comprehensive course covers both front-end and back-end web development, 
+    offering a deep dive into the essential tools and technologies required to build modern, scalable web applications.`,
   },
 ];
 
@@ -59,9 +69,9 @@ export const PROJECTS = [
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["Clerk", "Xata", "NextJs", "DrizzzleORM","Shadcn/ui"],
+    technologies: ["Clerk", "Xata", "NextJs", "DrizzzleORM", "Shadcn/ui"],
   },
- 
+
   {
     title: "Store it",
     image: project3,
